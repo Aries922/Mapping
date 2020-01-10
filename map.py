b86b4a45e7376ff5a746f332a7d9bdacbc2ac116
villen2002 <sumitsngh1698@gmail.com>
2020-01-10 11:17:02 +0530

    second commit

--- map.py
+++ map.py
@@ -5,9 +5,10 @@ data=pandas.read_csv("/home/sumit/mapping/Volcanoes_USA.txt")
 City=list(data["LOCATION"])
 Lat=list(data["LAT"])
 log=list(data["LON"])
+
 map=folium.Map([28.61, 77.21],zoom_starts=8,Tiles="Mapbox bright")
 fg=folium.FeatureGroup(Name="My Map")
 for ct, lt,ln in zip(City,Lat,log):
-    fg.add_child(folium.Marker(location=[lt,ln],popup=ct,icon=folium.Icon(colour='blue')))
+    fg.add_child(folium.Marker(location=[lt,ln],popup=ct,icon=folium.Icon(colour='green')))
 map.add_child(fg)
 map.save("Map1.html")
